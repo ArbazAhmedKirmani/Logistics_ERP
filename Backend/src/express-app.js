f9519eba72d722d3ef7c9613ef933f26e129eeb0
syed.arbaz <syed.arbaz@DESKTOP-0AAC4DC>
2023-03-06 04:09:18 +0500

    changes

--- Backend/src/express-app.js
+++ Backend/src/express-app.js
@@ -5,11 +5,16 @@ const expressFileupload = require("express-fileupload");
 const { authentication } = require("./api");
 
 module.exports = async (app) => {
-    
+  // Global Variables
+  global.__basedir = __dirname;
+
   // Express Setup
   app.use(express.json({ limit: "50mb" }));
   app.use(express.static(__dirname + "/public"));
-  app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+  app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+
+  // Express File-Upload
+  app.use(expressFileupload());
 
   // Cors Setup
   app.use(cors({ origin: "*" }));
@@ -21,5 +26,5 @@ module.exports = async (app) => {
   authentication(app);
 
   // error handling
-  //   app.use(HandleErrors)
+  // app.use(HandleErrors);
 };
